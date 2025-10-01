f4c1fd5f634898edf08654eacd860f9cf47fc55d
minabayat <minabayatt9@gmail.com>
2025-10-01 20:32:52 +0330

    v2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,36 @@
 import './style.css';
- 
+const slidesEL = document.getElementsByClassName("slide")
+const btmNext = document.getElementById("next")
+const btmPre = document.getElementById("prev")
+
+btmNext.addEventListener("click", () => {
+    const firstEl = document.querySelector(".first")
+    firstEl.classList.remove("first")
+    if (firstEl.nextElementSibling) {
+        firstEl.nextElementSibling.classList.add("first")
+
+    } else {
+        slidesEL[0].classList.add("first")
+    }
+})
+
+btmPre.addEventListener("click", () => {
+    const firstEl = document.querySelector(".first")
+    firstEl.classList.remove("first")
+    if (firstEl.previousElementSibling) {
+        firstEl.previousElementSibling.classList.add("first")
+
+    } else {
+        slidesEL[slidesEL.length - 1].classList.add("first")
+    }
+})
+
+setInterval(() => {
+  const firstEl = document.querySelector(".first");
+  firstEl.classList.remove("first");
+  if (currentEl.nextElementSibling) {
+    currentEl.nextElementSibling.classList.add("first");
+  } else {
+    slideEl[0].classList.add("first");
+  }
+}, 5000);
